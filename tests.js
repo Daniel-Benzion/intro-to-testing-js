26e3b3a15ab5d43d7943588d31d5a24bcca94966
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-11-12 00:32:24 -0500

    first refactor

--- tests.js
+++ tests.js
@@ -15,19 +15,28 @@ describe('helloWorld', function() {
 });
 
 describe('sayHello', function () {
-    it('should be a defined function', function() {
+    it('should be a defined function', function () {
         expect(typeof sayHello).toBe('function');
     });
-    it('should return a string when called', function() {
+    it('should return a string when called', function () {
         expect(typeof sayHello()).toBe('string');
     });
-    it('should return the string "Hello, Jane!"', function() {
-        expect(sayHello("Jane")).toBe("Hello, Jane!");
-    });
+});
+
+describe('sayHello("Jane")', function() {
+        it('should return the string "Hello, Jane!"', function() {
+            expect(sayHello("Jane")).toBe("Hello, Jane!");
+        });
+})
+
+describe('sayHello("Alex")', function() {
     it('should return the string "Hello, Alex!"', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
+});
+
+describe('sayHello("Pat")', function() {
     it('should return the string "Hello, Pat!"', function() {
         expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
-})
+});
