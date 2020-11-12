cff0c7baf3575f573b5a1c47cef2424cdafab752
Daniel Benzion <daniel.z.benzion@gmail.com>
2020-11-12 00:34:59 -0500

    second refactor

--- code.js
+++ code.js
@@ -3,16 +3,6 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
-const sayHello = function(n) {
-    switch (n) {
-        case "Alex":
-            return "Hello, Alex!";
-            break;
-        case "Pat":
-            return "Hello, Pat!";
-            break;
-        default:
-            return "Hello, Jane!";
-            break;
-    }
+const sayHello = function(name) {
+    return "Hello, " + name + "!";
 }
